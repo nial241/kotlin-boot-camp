0edd6f23298bd8b5619b16eb9bf15763f830185d
nial241 <an.shchurova011@yandex.ru>
2018-10-01 23:59:12 +0300

    assignment01 : in Kotlin style [v2:test-OK]

--- assignments/01/src/main/kotlin/io/rybalkinsd/kotlinbootcamp/assignments/util.kt
+++ assignments/01/src/main/kotlin/io/rybalkinsd/kotlinbootcamp/assignments/util.kt
@@ -6,10 +6,7 @@ package io.rybalkinsd.kotlinbootcamp.assignments
  * @param values an argument. !! Assume values.length > 0. !!
  * @return the largest of values.
  */
-fun max(values: List<Int>): Int = if (values.isNotEmpty()) {
-    var maxValue: Int = values[0]
-    values.forEach { if (it > maxValue) maxValue = it }
-    maxValue } else 0
+fun max(values: List<Int>): Int = values.max() ?: 0
 
 /**
  * Returns the sum of all `int` values.
@@ -17,7 +14,4 @@ fun max(values: List<Int>): Int = if (values.isNotEmpty()) {
  * @param values an argument. Assume values.length > 0.
  * @return the sum of all values.
  */
-fun sum(values: List<Int>): Long = if (values.isNotEmpty()) {
-    var sum: Long = 0
-    values.forEach { sum += it }
-    sum } else 0
+fun sum(values: List<Int>): Long = values.sumByDouble { it.toDouble() }.toLong()
