ef66ec54d2c381df3b5d88b0cf5872767da7ef4d
nial241 <an.shchurova011@yandex.ru>
2018-10-07 18:58:49 +0300

    assignment02: test fixed [v1:test-OK]

--- assignments/02/src/main/kotlin/io/rybalkinsd/kotlinbootcamp/geometry/Geometry.kt
+++ assignments/02/src/main/kotlin/io/rybalkinsd/kotlinbootcamp/geometry/Geometry.kt
@@ -10,9 +10,18 @@ interface Collider {
 /**
  * 2D point with integer coordinates
  */
-class Point(x: Int, y: Int) : Collider {
-    override fun isColliding(other: Collider): Boolean {
-        TODO("not implemented")
+class Point(val x: Int, val y: Int) : Collider {
+    override fun isColliding(other: Collider): Boolean = when (other) {
+        is Point -> this == other
+        is Bar -> other.isColliding(this)
+        else -> false
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+        other as Point
+        return x == other.x && y == other.y
     }
 }
 
@@ -23,7 +32,26 @@ class Point(x: Int, y: Int) : Collider {
  * (It does not matter, which opposite corners you choose to define bar)
  */
 class Bar(firstCornerX: Int, firstCornerY: Int, secondCornerX: Int, secondCornerY: Int) : Collider {
-    override fun isColliding(other: Collider): Boolean {
-        TODO("not implemented")
+
+    fun isPointInsideBar(pointX: Int, pointY: Int) = (pointX >= cornersInOrder[0] && pointX <= cornersInOrder[1] && pointY >= cornersInOrder[2] && pointY <= cornersInOrder[3])
+
+    fun isBarCollidingBar(otherBar: Bar) = isPointInsideBar(otherBar.cornersInOrder[0], otherBar.cornersInOrder[2]) || isPointInsideBar(otherBar.cornersInOrder[0], otherBar.cornersInOrder[3]) || isPointInsideBar(otherBar.cornersInOrder[1], otherBar.cornersInOrder[2]) || isPointInsideBar(otherBar.cornersInOrder[1], otherBar.cornersInOrder[3])
+
+    override fun isColliding(other: Collider) =
+        when {
+            other == this -> true
+            other is Point -> isPointInsideBar(other.x, other.y)
+            other is Bar -> isBarCollidingBar(other) || other.isBarCollidingBar(this)
+            else -> false
+        }
+
+    var cornersInOrder = listOf(firstCornerX, secondCornerX).sorted() + listOf(firstCornerY, secondCornerY).sorted()
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+        other as Bar
+        if (cornersInOrder != other.cornersInOrder) return false
+        return true
     }
 }
